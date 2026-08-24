4413214b6c55f2e972377fadd913f4fce307f623
0x6d61 <0x6d61@gmail.com>
2026-08-24 14:49:07 +0900

    chore: bootstrap Maven project

---
+++ src/test/java/io/github/tapo/c219/ProjectSmokeTest.java
@@ -0,0 +1,14 @@
+package io.github.tapo.c219;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectSmokeTest {
+    @Test
+    void runsOnTheSupportedJavaVersion() {
+        assertTrue(
+                Runtime.version().feature() >= 21,
+                "This project requires Java 21 or newer");
+    }
+}
